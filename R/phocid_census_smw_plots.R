3ad20c36c239700d34b51cfb4802584f6da9dde3
Sam Woodman <sam.woodman@noaa.gov>
2023-06-02 16:45:45 -0700

    Making some plots

--- R/phocid_census_smw_plots.R
+++ R/phocid_census_smw_plots.R
@@ -1,47 +1,190 @@
+# Make plots!
+
 library(tidyverse)
 library(here)
 library(amlrPinnipeds)
 library(glue)
 library(ggExtra)
 
-### Read in data
-con <- amlr_dbConnect(Database = "***REMOVED***")
 
-x <- read.csv(here("data/cs_combined_data/phocids_cs_combined_long.csv"), 
-              na.strings = "") %>% 
-  mutate(census_date = as.Date(census_date))
-x.header <- read.csv(here("data/cs_combined_data/phocids_cs_combined_header.csv"))
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Read in data, and do some prep
+z.header <- read_csv(here("output", "cs_phocid_census_header.csv"), 
+                     col_types = "ccDDilc") %>% 
+  data.frame()
+
+z <- read_csv(here("output", "cs_phocid_census_records.csv"), 
+              col_types = "cccciiiiiiiiiii") %>% 
+  data.frame()
+
+# season.name.levels <- paste(
+#   1997:2022, str_pad(c(98, 99, 00, 1:23), width = 2, pad = "0"), sep = "/"
+# )
+season.name.levels.inach <- paste(
+  1997:2008, str_pad(c(98, 99, 00, 1:09), width = 2, pad = "0"), sep = "/"
+)
+season.name.levels.amlr <- paste(
+  2009:2022, str_pad(c(10:23), width = 2, pad = "0"), sep = "/"
+)
+season.name.levels <- c(season.name.levels.inach, season.name.levels.amlr)
 
 
-### Let's go exploring!
-x.summ.date <- x %>% 
-  group_by(season_name) %>% 
-  summarise(n_dates = n_distinct(census_date), 
-            min_date = min(census_date), 
-            max_date = max(census_date))
-
-
-### Make a visualization of census dates by year from the header data
-d <- x.header %>% 
-  mutate(season_name = as.factor(season_name), 
+header.toplot <- z.header %>% 
+  mutate(season_name = fct(season_name, levels = season.name.levels), 
          season_int = as.numeric(substr(season_name, 1, 4)), 
          research_program = as.factor(research_program), 
          census_date_start = as.Date(census_date_start), 
          census_date_end = as.Date(census_date_end), 
-         # census_month_day = format(census_date_start, "%b-%d"), 
-         # census_yday = yday(census_date_start), 
-         tmp_year = if_else(month(census_date_start) >= 7, 1999, 2000), 
-         tmp_date = mdy(format(census_date_start, glue("%m-%d-{tmp_year}"))))
+         plot_year = if_else(month(census_date_start) >= 7, 1999, 2000), 
+         plot_date_start = mdy(format(census_date_start, 
+                                      glue("%m-%d-{plot_year}"))))
 
 
-g <- ggplot(d, aes(x = tmp_date, y = season_int)) +
-  geom_point() +
-  labs(title = "Dates of Cape Shirreff phocid census by season", 
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+### Make a visualization of census dates by year from the header data
+g.1 <- ggplot(header.toplot, aes(x = plot_date_start, y = season_int)) +
+  geom_point(aes(col = research_program)) +
+  labs(title = "Cape Shirreff phocid census dates, by season", 
        x = "Date", y = "Season") +
-  theme(axis.text.x = element_text(angle = 90)) +
+  theme(axis.text.x = element_text(angle = 90),
+        legend.position = "top") +
+  guides(color = guide_legend(title = "Research program")) + 
   scale_x_date(date_breaks = "1 week", date_labels = "%b %d") + 
-  scale_y_continuous(breaks = unique(d$season_int), labels = unique(d$season_name))
-g
-g <- ggMarginal(g, type = "histogram", fill="transparent", margins = "y")
-g
-ggsave(here("output", "census_timing.png"), g, width = 10, height = 6)
+  # scale_y_discrete(drop=FALSE)
+  scale_y_continuous(breaks = 1997:2022,
+                     labels = season.name.levels)
+g.1
+g.1 <- ggMarginal(g.1, type = "histogram", fill="transparent", margins = "y")
+g.1
+ggsave(here("output", "census_timing.png"), g.1, width = 10, height = 6)
+rm(g.1)
+
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Join with header.toplot to get variables for plot axes
+z.toplot <- z %>% 
+  left_join(header.toplot, by = join_by(header_id))
+
+# i <- amlrPinnipeds::pinniped.phocid.sp[1]
+
+
+#-------------------------------------------------------------------------------
+# Core census locations
+z.toplot.core <- z.toplot %>% 
+  filter(location == "Core census locations")
+
+
+### Dot counts
+# Try same as above, with counts controlled by dot size
+for (i in amlrPinnipeds::pinniped.phocid.sp) {
+  print(i)
+  if (i == 'Elephant seal') i <- "Southern elephant seal"
+  i.toplot <- z.toplot.core %>% filter(species_common == i)
+  
+  g.curr <- ggplot(i.toplot, aes(x = plot_date_start, y = season_name)) +
+    geom_point(aes(size = total_count)) +
+    labs(title = paste("Cape Shirreff phocid census counts", 
+                       "core census locations", i, sep = " - "),
+         x = "Date", y = "Season") +
+    theme(axis.text.x = element_text(angle = 90)) +
+    guides(size = guide_legend(title = "Count")) + 
+    scale_x_date(date_breaks = "1 week", date_labels = "%b %d") +
+    scale_y_discrete(drop=FALSE)
+  
+  i.filename <- str_replace(tolower(i), " ", "_")
+  ggsave(here("output", "scatterplot", paste0(i.filename, ".png")), 
+         g.curr, width = 10, height = 6)
+}; rm(i, i.toplot, g.curr)
+
+
+### Line charts
+# Try same as above, with counts controlled by dot size
+for (i in amlrPinnipeds::pinniped.phocid.sp) {
+  print(i)
+  if (i == 'Elephant seal') i <- "Southern elephant seal"
+  i.toplot <- z.toplot.core %>% filter(species_common == i)
+  
+  g.curr <- ggplot(i.toplot, aes(x = plot_date_start, y = total_count)) +
+    geom_point(aes(color = season_name)) +
+    geom_line(aes(color = season_name)) + 
+    labs(title = paste("Cape Shirreff phocid census counts", 
+                       "core census locations", i, sep = " - "),
+         x = "Date", y = "Count") +
+    theme(axis.text.x = element_text(angle = 90)) +
+    guides(color = guide_legend(title = "Season")) + 
+    scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+  g.curr
+  
+  i.filename <- str_replace(tolower(i), " ", "_")
+  ggsave(here("output", "line_graph", paste0(i.filename, ".png")), 
+         g.curr, width = 10, height = 6)
+}; rm(i, i.toplot, g.curr)
+
+
+#-------------------------------------------------------------------------------
+# Core census locations + PST; from 2009/10 on
+cspc_sum <- function(x, na.rm = TRUE) {
+  if_else(all(is.na(x)), NA_integer_, sum(x, na.rm = na.rm))
+}
+
+z.toplot.combo <- z %>% 
+  group_by(header_id, species_common) %>% 
+  summarise(location = "Core + PST", 
+            across(ends_with("_count"), cspc_sum),  
+            .groups = "drop") %>% 
+  left_join(header.toplot, by = join_by(header_id)) %>% 
+  filter(census_date_start > ymd("2009-07-01"), 
+         surveyed_san_telmo) %>% 
+  mutate(season_name = as.character(season_name), 
+         season_name = fct(season_name, levels = season.name.levels.amlr))
+  
+
+
+### Dot counts
+# Try same as above, with counts controlled by dot size
+for (i in amlrPinnipeds::pinniped.phocid.sp) {
+  print(i)
+  if (i == 'Elephant seal') i <- "Southern elephant seal"
+  i.toplot <- z.toplot.combo %>% filter(species_common == i)
+  
+  g.curr <- ggplot(i.toplot, aes(x = plot_date_start, y = season_name)) +
+    geom_point(aes(size = total_count)) +
+    labs(title = paste("Cape Shirreff phocid census counts", 
+                       "core+pst locations", i, sep = " - "),
+         x = "Date", y = "Season") +
+    theme(axis.text.x = element_text(angle = 90)) +
+    guides(size = guide_legend(title = "Count")) + 
+    scale_x_date(date_breaks = "1 week", date_labels = "%b %d") +
+    scale_y_discrete(drop=FALSE)
+  
+  i.filename <- paste0("core+pst_", str_replace(tolower(i), " ", "_"))
+  ggsave(here("output", "scatterplot", paste0(i.filename, ".png")), 
+         g.curr, width = 10, height = 6)
+}; rm(i, i.toplot, g.curr)
+
+
+### Line charts
+# Try same as above, with counts controlled by dot size
+for (i in amlrPinnipeds::pinniped.phocid.sp) {
+  print(i)
+  if (i == 'Elephant seal') i <- "Southern elephant seal"
+  i.toplot <- z.toplot.combo %>% filter(species_common == i)
+  
+  g.curr <- ggplot(i.toplot, aes(x = plot_date_start, y = total_count)) +
+    geom_point(aes(color = season_name)) +
+    geom_line(aes(color = season_name)) + 
+    labs(title = paste("Cape Shirreff phocid census counts", 
+                       "core+pst locations", i, sep = " - "),
+         x = "Date", y = "Count") +
+    theme(axis.text.x = element_text(angle = 90)) +
+    guides(color = guide_legend(title = "Season")) + 
+    scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+  g.curr
+  
+  i.filename <- paste0("core+pst_", str_replace(tolower(i), " ", "_"))
+  ggsave(here("output", "line_graph", paste0(i.filename, ".png")), 
+         g.curr, width = 10, height = 6)
+}; rm(i, i.toplot, g.curr)
